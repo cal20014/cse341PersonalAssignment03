d42ad5931450a8e5dd0d4d67e30504e97ad33a10
cal20014 <cal20014@byui.edu>
2024-02-01 10:40:18 -0700

    Update routes and add Swagger documentation

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
-require("dotenv").config();
-
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger-output.json");
 const express = require("express");
+
 const app = express();
 const connectToDatabase = require("./db/connect.js");
+
 const port = process.env.PORT || 8080;
 const host = process.env.HOST || "localhost";
+
+const dotenv = require("dotenv");
+dotenv.config();
 
 app.use(express.json()); // Used to parse JSON bodies
 app.use(express.urlencoded({ extended: true })); // Used to parse URL-encoded bodies
@@ -19,6 +24,7 @@ connectToDatabase()
   });
 
 app.use("/", require("./routes"));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 if (process.env.NODE_ENV === "development") {
   app.listen(port, host, () => {
